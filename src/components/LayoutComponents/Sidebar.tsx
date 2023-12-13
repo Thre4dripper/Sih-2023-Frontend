e52e3f20cd08ae7ca5ec478db63089324da3589e
Bilal <bsajid173@gmail.com>
2023-12-13 19:44:56 +0530

    fix

--- src/components/LayoutComponents/Sidebar.tsx
+++ src/components/LayoutComponents/Sidebar.tsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 const LeftSidebar = ({ links }: LProps) => {
   return (
     <div
-      className={`flex h-screen sticky top-0 max-lg:w-auto w-[20%] flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-gray-50 pl-6`}
+      className={`flex h-screen sticky top-0 max-lg:w-auto w-[20%] flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white pl-6`}
     >
       <div className="flex justify-between h-16 shrink-0 items-center pr-4">
         <img
@@ -25,8 +25,8 @@ const LeftSidebar = ({ links }: LProps) => {
                     to={item.target}
                     className={({ isActive }) =>
                       isActive
-                        ? `bg-gray-500 text-gray-50 group flex max-lg:justify-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold`
-                        : `text-gray-500 hover:text-gray-200 hover:bg-gray-400 group flex max-lg:justify-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold`
+                        ? `bg-gray-50 text-indigo-600 group flex max-lg:justify-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold`
+                        : `text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex max-lg:justify-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold`
                     }
                   >
                     {({ isActive }) => (
@@ -34,8 +34,8 @@ const LeftSidebar = ({ links }: LProps) => {
                         <span
                           className={`${
                             isActive
-                              ? "text-gray-50 h-6 w-6 shrink-0"
-                              : "text-gray-500 group-hover:text-gray-200 h-6 w-6 shrink-0"
+                              ? "text-indigo-600 h-6 w-6 shrink-0"
+                              : "text-gray-400 group-hover:text-indigo-600 h-6 w-6 shrink-0"
                           }`}
                         >
                           {item.icon}
