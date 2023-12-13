27d4146c02f773a4a0cdc5ade1950687787b6157
Bilal <bsajid173@gmail.com>
2023-12-13 21:02:01 +0530

    added bar and line charts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
+import LineChart from "@/components/Charts/BarChart";
 import Layout from "@/components/LayoutComponents/Layout";
 
 const Home = () => {
   return (
     <Layout>
-      <div className="h-[200vh]">Hi</div>
+      <div className="h-[200vh]">
+        <LineChart />
+      </div>
     </Layout>
   );
 };
