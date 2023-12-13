7404248998f2931ee1d1721fda54bba390ef6060
Bilal <bsajid173@gmail.com>
2023-12-13 18:38:00 +0530

    fixes and shit

--- src/components/LayoutComponents/Navbar.tsx
+++ src/components/LayoutComponents/Navbar.tsx
@@ -9,40 +9,62 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { NProps } from "./LayoutProps";
+import { ArrowRightFromLine } from "lucide-react";
+import { useSetRecoilState, useRecoilValue } from "recoil";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Link } from "react-router-dom";
+import { sidebarState } from "@/atoms/sidebarState";
 
 const Navbar = ({ email, name, links }: NProps) => {
+  const setSidebarHidden = useSetRecoilState(sidebarState);
+  const isHidden = useRecoilValue(sidebarState);
+
   return (
-    <div className="w-full p-4 px-6 flex justify-end border-b border-gray-200">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Avatar className="cursor-pointer">
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56">
-          <DropdownMenuLabel>{name}</DropdownMenuLabel>
-          <DropdownMenuLabel>{email}</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            {links?.map((item) => {
-              return (
-                <DropdownMenuItem>
-                  <Link to={item.target}>
-                    <span className="flex gap-x-2">
-                      {item.icon}
-                      {item.title}
-                    </span>
-                  </Link>
-                </DropdownMenuItem>
-              );
-            })}
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+    <div className="w-full p-4 px-4 flex border-b border-gray-200">
+      <div className="flex items-center">
+        <span
+          onClick={() =>
+            setSidebarHidden((_prev) => {
+              return { isHidden: false };
+            })
+          }
+          className={` rounded-sm p-2 ${
+            !isHidden?.isHidden ? "hidden" : ""
+          } max-lg:block hover:bg-gray-50 cursor-pointer`}
+        >
+          <ArrowRightFromLine className={`text-gray-700`} />
+        </span>
+      </div>
+      <div className="ml-auto">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar className="cursor-pointer">
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel>{name}</DropdownMenuLabel>
+            <DropdownMenuLabel>{email}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              {links?.map((item) => {
+                return (
+                  <DropdownMenuItem>
+                    <Link to={item.target}>
+                      <span className="flex gap-x-2">
+                        {item.icon}
+                        {item.title}
+                      </span>
+                    </Link>
+                  </DropdownMenuItem>
+                );
+              })}
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
--- src/components/LayoutComponents/Sidebar.tsx
+++ src/components/LayoutComponents/Sidebar.tsx
@@ -2,10 +2,19 @@
 import { LProps } from "./LayoutProps";
 import { NavLink } from "react-router-dom";
 import { ArrowLeftFromLine } from "lucide-react";
+import { sidebarState } from "@/atoms/sidebarState";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 const LeftSidebar = ({ links }: LProps) => {
+  const setSidebarHidden = useSetRecoilState(sidebarState);
+  const isHidden = useRecoilValue(sidebarState);
+
   return (
-    <div className="flex h-screen sticky top-0 w-[20%] flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white pl-6">
+    <div
+      className={`flex h-screen sticky top-0 ${
+        isHidden?.isHidden ? "w-auto" : ""
+      } max-lg:w-auto w-[20%] flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white pl-6`}
+    >
       <div className="flex justify-between h-16 shrink-0 items-center pr-4">
         <img
           className="h-8 w-auto"
@@ -13,8 +22,17 @@ const LeftSidebar = ({ links }: LProps) => {
           alt="Your Company"
         />
         {/* icon to collapse the sidebar */}
-        <span className="p-2 rounded-sm hover:bg-gray-50 cursor-pointer">
-          <ArrowLeftFromLine className="text-gray-700" />
+        <span
+          onClick={() =>
+            setSidebarHidden((_prev) => {
+              return { isHidden: true };
+            })
+          }
+          className={`p-2 rounded-sm  ${
+            isHidden?.isHidden ? "hidden" : ""
+          } max-lg:hidden hover:bg-gray-50 cursor-pointer`}
+        >
+          <ArrowLeftFromLine className={`text-gray-700`} />
         </span>
       </div>
       <nav className="flex flex-1 flex-col pr-6">
@@ -27,8 +45,12 @@ const LeftSidebar = ({ links }: LProps) => {
                     to={item.target}
                     className={({ isActive }) =>
                       isActive
-                        ? "bg-gray-50 text-indigo-600 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                        : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                        ? `bg-gray-50 text-indigo-600 group flex ${
+                            isHidden?.isHidden ? "justify-center" : ""
+                          } max-lg:justify-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold`
+                        : `text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex ${
+                            isHidden?.isHidden ? "justify-center" : ""
+                          } max-lg:justify-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold`
                     }
                   >
                     {({ isActive }) => (
@@ -42,7 +64,13 @@ const LeftSidebar = ({ links }: LProps) => {
                         >
                           {item.icon}
                         </span>
-                        {item.title}
+                        <p
+                          className={`text-light-1 ${
+                            isHidden?.isHidden ? "hidden" : ""
+                          } max-lg:hidden`}
+                        >
+                          {item.title}
+                        </p>
                       </>
                     )}
                   </NavLink>
